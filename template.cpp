5a7634bbccf64ec134f68837a4508294edd52063
leoli1102 <102196319+leoli1102@users.noreply.github.com>
2022-11-11 18:10:17 -0500

    Add files via upload
    This is the general template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+//types
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+#define f first
+#define s second
+//loops 
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define ROF(i,a,b) for (int i = (b) - 1; i >= (a); i--)
+#define cout(x) std::cout << std::fixed << std::setprecision(x) 
+
+
+//---------------------------------------------------------<File input and output>---------------------------------------------------------
+#define IO(NAME) \
+    cin.tie(0)->sync_with_stdio(0); \
+    if(fopen(NAME ".in","r")) freopen(NAME ".in","r",stdin), \
+    freopen(NAME ".out","w",stdout); 
+
+void solve(){
+	
+}
+
+int main() {
+    //IO(""); // put the name here
+    // put code here
+    solve();
+    return 0;
+}
